00b8c7466a700f6e695522bf043d36b298aa4161
fb22 <fb22182@gmail.com>
2025-02-06 06:00:22 +0100

    Initial state

---
+++ Assets/CodebugLounge/Scripts/CodebugFeeder.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.AI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CodebugFeeder : UdonSharpBehaviour
+{
+    public NavMeshAgent[] codebugs;
+
+    public Transform randomCenter;
+    
+    float timer = 0;
+    
+    void Start()
+    {
+        InteractionText = "Feed Codebugs";
+        DisableInteractive = false;
+    }
+    
+    public override void Interact()
+    {
+        foreach (NavMeshAgent codebug in codebugs)
+        {
+            codebug.SetDestination(transform.position);
+            timer = 8;
+        }
+    }
+    
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer<0)
+        {
+            foreach (NavMeshAgent codebug in codebugs)
+            {
+                Vector3 randomDirection = Random.insideUnitSphere * 13;
+                codebugs[Random.Range(0,codebugs.Length)].SetDestination(randomCenter.position + randomDirection);
+            }
+            
+            timer = 1;
+        }
+    }
+}
