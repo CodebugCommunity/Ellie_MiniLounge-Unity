f16995d4707f35bfb6555f58c1de5aa3c6bfb705
fb22 <fb22182@gmail.com>
2025-02-18 02:36:36 +0100

    World toggles, fixed some collisions

---
+++ Assets/CodebugLounge/Scripts/FactorySwitch.cs
@@ -0,0 +1,34 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FactorySwitch : UdonSharpBehaviour
+{
+    
+    public bool state = false;
+    Transform switchTransform;
+    
+    public GameObject[] ObjectsToToggle;
+    
+    public override void Interact()
+    {
+        state = !state;
+        UpdateVisuals();
+        foreach (GameObject o in ObjectsToToggle)
+        {
+            o.SetActive(state);
+        }
+    }
+    
+    void UpdateVisuals()
+    {
+        switchTransform.localRotation = Quaternion.Euler(state ? -90 : 90, 0,0);
+    }
+    
+    void Start()
+    {
+        switchTransform = transform.GetChild(1);
+    }
+}
---
+++ Assets/CodebugLounge/Scripts/RugSwitch.cs
@@ -0,0 +1,34 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RugSwitch : UdonSharpBehaviour
+{
+    public bool state = false;
+    Transform switchTransform;
+    
+    public MeshRenderer[] ObjectsToToggle;
+    
+    public override void Interact()
+    {
+        state = !state;
+        UpdateVisuals();
+        foreach (var o in ObjectsToToggle)
+        {
+            o.sharedMaterial.SetFloat("_LayerCount", state ? 3 : 1);
+        }
+    }
+    
+    void UpdateVisuals()
+    {
+        switchTransform.localRotation = Quaternion.Euler(state ? -90 : 90, 0,0);
+    }
+    
+    void Start()
+    {
+        switchTransform = transform.GetChild(1);
+        UpdateVisuals();
+    }
+}
