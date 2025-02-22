68c3914f8dc035831c99a3ae232244800dd56eda
fb22 <fb22182@gmail.com>
2025-02-22 06:02:28 +0100

    Mirror room, large beanbags, better performance

---
+++ Assets/CodebugLounge/Scripts/Teleportation.cs
@@ -0,0 +1,33 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Teleportation : UdonSharpBehaviour
+{
+    
+    [SerializeField] Transform teleportTarget;
+    [SerializeField] GameObject[] objToEnable;
+    [SerializeField] GameObject[] objToDisable;
+    
+    void Start()
+    {
+        
+    }
+    
+    public override void Interact()
+    {
+        Networking.LocalPlayer.TeleportTo(teleportTarget.position, teleportTarget.rotation);
+
+        foreach (var obj in objToEnable)
+        {
+            obj.SetActive(true);
+        }
+        
+        foreach (var obj in objToDisable)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
