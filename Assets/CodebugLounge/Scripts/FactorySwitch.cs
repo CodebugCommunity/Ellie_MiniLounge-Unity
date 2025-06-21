0b1e8c7aa9b2645b8879151647f0391dd06bb4df
fb22 <fb22182@gmail.com>
2025-06-21 03:10:00 +0200

    Chairs! Mirror room dev progress showcase, lightmap improvements

--- Assets/CodebugLounge/Scripts/FactorySwitch.cs
+++ Assets/CodebugLounge/Scripts/FactorySwitch.cs
@@ -3,6 +3,55 @@ using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+using VRCStation = VRC.SDK3.Components.VRCStation;
+
+#if UNITY_EDITOR
+using UnityEditor;
+[CustomEditor(typeof(FactorySwitch))]
+public class EventButtonEditor : Editor
+{
+    bool debugMode = false;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        FactorySwitch eventButton = (FactorySwitch)target;
+        
+        
+        debugMode = GUILayout.Toggle(debugMode, "Debug toggle");
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("Update seats"))
+        {
+            
+            
+            var seats = GameObject.FindObjectsByType<VRCStation>(FindObjectsSortMode.None);
+            Collider[] seatColliders= new Collider[seats.Length];
+            for (int i = 0; i < seats.Length; i++)
+            {
+                seatColliders[i] = seats[i].GetComponent<Collider>();
+            }
+            eventButton.ObjectsToToggle = seatColliders;
+            
+            if(debugMode)
+            {
+                foreach (var collider in seatColliders)
+                {
+                    collider.transform.GetChild(2).gameObject.SetActive(true);
+                }
+            }else 
+            {
+                foreach (var collider in seatColliders)
+                {
+                    collider.transform.GetChild(2).gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
+#endif
+
 
 public class FactorySwitch : UdonSharpBehaviour
 {
@@ -10,17 +59,17 @@ public class FactorySwitch : UdonSharpBehaviour
     public bool state = false;
     Transform switchTransform;
     
-    public GameObject[] ObjectsToToggle;
+    public Collider[] ObjectsToToggle;
     
     public override void Interact()
     {
         state = !state;
         UpdateVisuals();
-        foreach (GameObject o in ObjectsToToggle)
+        foreach (Collider o in ObjectsToToggle)
         {
             if (o != null)
             {
-                o.SetActive(state);
+                o.enabled = state;
             }
             
         }
