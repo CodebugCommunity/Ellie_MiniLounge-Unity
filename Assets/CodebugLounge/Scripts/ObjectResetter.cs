6cee59ac33be7ec4c681e65b34b6e33eb76a86e9
fb22 <fb22182@gmail.com>
2025-07-06 07:48:06 +0200

    Reseting, settings rework, tunnel access

---
+++ Assets/CodebugLounge/Scripts/ObjectResetter.cs
@@ -0,0 +1,77 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using VRC.SDK3.Components;
+
+[CustomEditor(typeof(ObjectResetter))]
+public class ObjectResetterEditor : Editor
+{
+    bool debugMode = false;
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        ObjectResetter resetter = (ObjectResetter)target;
+        
+        
+        //debugMode = GUILayout.Toggle(debugMode, "Debug toggle");
+
+        GUILayout.Space(10);
+
+        if (GUILayout.Button("Update seats"))
+        {
+            
+            
+            var interactables = GameObject.FindObjectsByType<VRCPickup>(FindObjectsSortMode.None);
+            GameObject[] interactableObjs= new GameObject[interactables.Length];
+            for (int i = 0; i < interactables.Length; i++)
+            {
+                interactableObjs[i] = interactables[i].gameObject;
+            }
+            
+            resetter.objectsToReset = interactableObjs;
+            
+            
+        }
+    }
+}
+#endif
+
+public class ObjectResetter : UdonSharpBehaviour
+{
+    [SerializeField] public GameObject[] objectsToReset;
+    [SerializeField] private Vector3[] startingPositions;
+    [SerializeField] private Quaternion[] startingRotations;
+    void Awake()
+    {
+        startingPositions = new Vector3[objectsToReset.Length];
+        startingRotations = new Quaternion[objectsToReset.Length];
+        
+        for (int i = 0; i < objectsToReset.Length; i++)
+        {
+            startingPositions[i] = objectsToReset[i].transform.position;
+            startingRotations[i] = objectsToReset[i].transform.rotation;
+        }
+    }
+    
+    public override void Interact()
+    {
+        for (int i = 0; i < objectsToReset.Length; i++)
+        {
+            objectsToReset[i].transform.position = startingPositions[i];
+            objectsToReset[i].transform.rotation = startingRotations[i];
+        }
+        Debug.Log("Reset objects to starting positions and rotations.");
+    }
+}
+
+struct TransformData
+{
+    public Vector3 position;
+    public Quaternion rotation;
+}
---
+++ Assets/CodebugLounge/Scripts/ObstacleLights.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ObstacleLights : UdonSharpBehaviour
+{
+    
+    [SerializeField] private GameObject[] lights;
+    [SerializeField] private float timeout;
+    
+    float timer = 0.5f;
+    bool lightOn = false;
+    
+    void Start()
+    {
+        
+    }
+    
+    
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        
+        if (timer <= 0 && !lightOn)
+        {
+            timer = 0.2f;
+            foreach (var light in lights)
+            {
+                light.SetActive(true);
+            }
+            lightOn = true;
+        }
+        
+        if (timer <= 0 && lightOn)
+        {
+            timer = 2f;
+            foreach (var light in lights)
+            {
+                light.SetActive(false);
+            }
+            lightOn = false;
+        }
+    }
+}
