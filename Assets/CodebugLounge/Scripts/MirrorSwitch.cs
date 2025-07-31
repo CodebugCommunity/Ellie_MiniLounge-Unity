0a49f9730606d7aebeff2571d1ba3b335bf08dde
fb22 <fb22182@gmail.com>
2025-07-31 02:06:34 +0200

    Building plants, spider, album covers, bugfixes

---
+++ Assets/CodebugLounge/Scripts/MirrorSwitch.cs
@@ -0,0 +1,38 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MirrorSwitch : UdonSharpBehaviour
+{
+    public bool state = false;
+    Transform switchTransform;
+    
+    public GameObject[] ObjectsToToggle;
+    
+    public override void Interact()
+    {
+        state = !state;
+        UpdateVisuals();
+        foreach (GameObject o in ObjectsToToggle)
+        {
+            if (o != null)
+            {
+                o.SetActive(state);
+            }
+            
+        }
+    }
+    
+    void UpdateVisuals()
+    {
+        switchTransform.localRotation = Quaternion.Euler(state ? -45 : 45, 0,0);
+    }
+    
+    void Start()
+    {
+        switchTransform = transform.GetChild(1);
+        UpdateVisuals();
+    }
+}
